dec4e24f43a20623086478bf5768d33002928d95
Ayush Kumar Nath <developer.ayushknath@gmail.com>
2022-11-17 09:44:28 +0530

    Timer would not go off if not set

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,5 +1,7 @@
 import * as entities from "./entities.js";
 
+let isTimerSet = false;
+
 function showInputPanel() {
     entities.timerInputPanel.classList.add("show-input-panel");
 
@@ -12,16 +14,11 @@ function hideInputPanel(e) {
 
     // PREVENTS INPUT PANEL FROM HIDING IF INNER ELEMENTS ARE CLICKED
     for (let i = 0; i < 4; i++) {
-        if (targetElement === document.body)
-            break;
-        else if (targetElement.classList.contains("timer-input"))
-            return;
-        else if (targetElement.classList.contains("timer-set"))
-            break;
-        else if (!targetElement.parentElement.classList.contains("timer-input"))
-            targetElement = targetElement.parentElement;
-        else
-            return;
+        if (targetElement === document.body) break;
+        else if (targetElement.classList.contains("timer-input")) return;
+        else if (targetElement.classList.contains("timer-set")) break;
+        else if (!targetElement.parentElement.classList.contains("timer-input")) targetElement = targetElement.parentElement;
+        else return;
     }
 
     entities.timerInputPanel.classList.remove("show-input-panel");
@@ -58,23 +55,34 @@ function updateDisplay(e) {
     entities.hourDisplay.innerHTML = entities.hourInput.value;
     entities.minuteDisplay.innerHTML = entities.minuteInput.value;
     entities.secondDisplay.innerHTML = entities.secondInput.value;
+
+    if (parseInt(entities.hourInput.value) !== 0 || parseInt(entities.minuteInput.value) !== 0 || parseInt(entities.secondInput.value) !== 0) isTimerSet = true;
+    else isTimerSet = false;
+
     hideInputPanel(e);
 }
 
 function finishCountdown(isComplete = false) {
-    if (isComplete)
-        entities.timesUpAudio.play();
+    if (isComplete) entities.timesUpAudio.play();
 
     for (let i = 0; i < 999999; i++) {
         clearInterval(i);
     }
 
     entities.timerDisplay.addEventListener("click", showInputPanel);
-    entities.startTimer.style.display = "flex";
     entities.pauseTimer.style.display = "none";
+    entities.startTimer.style.display = "flex";
+
+    entities.hourInput.value = '0';
+    entities.minuteInput.value = '0';
+    entities.secondInput.value = '0';
+
+    isTimerSet = false;
 }
 
 function startCountdown() {
+    if (!isTimerSet) return;
+
     entities.timerDisplay.removeEventListener("click", showInputPanel);
     entities.startTimer.style.display = "none";
     entities.pauseTimer.style.display = "flex";
@@ -82,19 +90,14 @@ function startCountdown() {
     setInterval(() => {
         if (parseInt(entities.secondDisplay.innerHTML) > 0) {
             entities.secondDisplay.innerHTML = `${parseInt(entities.secondDisplay.innerHTML) - 1}`;
-        } else {
-            if (parseInt(entities.minuteDisplay.innerHTML) > 0) {
-                entities.minuteDisplay.innerHTML = `${parseInt(entities.minuteDisplay.innerHTML) - 1}`;
-                entities.secondDisplay.innerHTML = "59";
-            } else {
-                if (parseInt(entities.hourDisplay.innerHTML) > 0) {
-                    entities.hourDisplay.innerHTML = `${parseInt(entities.hourDisplay.innerHTML) - 1}`;
-                    entities.minuteDisplay.innerHTML = "59";
-                    entities.secondDisplay.innerHTML = "59";
-                } else finishCountdown(true);
-            }
-        }
-
+        } else if (parseInt(entities.minuteDisplay.innerHTML) > 0) {
+            entities.minuteDisplay.innerHTML = `${parseInt(entities.minuteDisplay.innerHTML) - 1}`;
+            entities.secondDisplay.innerHTML = "59";
+        } else if (parseInt(entities.hourDisplay.innerHTML) > 0) {
+            entities.hourDisplay.innerHTML = `${parseInt(entities.hourDisplay.innerHTML) - 1}`;
+            entities.minuteDisplay.innerHTML = "59";
+            entities.secondDisplay.innerHTML = "59";
+        } else finishCountdown(true);
     }, 1000);
 }
 
